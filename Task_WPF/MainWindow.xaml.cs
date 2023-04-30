93ce608abaf0bc16a67b46a1b5e5f801c667e980
Leonardo <leonardocasia34@hotmail.com>
2023-04-30 23:07:23 +0200

    Aggiungere i file di progetto.

---
+++ Task_WPF/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Task_WPF
+{
+    /// <summary>
+    ///  we can used a gif to show a loding 
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool flag = true;
+        int tot = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Processo2()
+        {
+            int sum = 0;
+
+            for (int i = 0; i < 150; i++)
+            {
+                await Task.Delay(800);
+                await Task.Run(() =>
+                {
+                    sum += i;
+                });
+
+                Timer.Text = $"{sum}  hai cliccato N{tot}";
+                await Task.Delay(200);
+                loadding.Stop();
+                loadding.Play();
+            }
+
+            MessageBox.Show("Loding completato");
+            loadding.Close();
+            loadding.Visibility = Visibility.Collapsed;
+            flag = true;
+        }
+
+        private void Button_Click_Counter(object sender, RoutedEventArgs e)
+        {
+            if (flag == true) {Processo2(); flag = false;}
+
+            tot++;
+        }
+
+
+    }
+}
